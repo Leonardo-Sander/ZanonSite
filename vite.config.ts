49f7c753f89e54667c1dd734c9523a505ef2a6dc
Leonardo Sander <leonardosander16@gmail.com>
2025-09-09 15:59:45 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ZanonSite/', // <-- coloque exatamente o nome do repositório aqui
 })
